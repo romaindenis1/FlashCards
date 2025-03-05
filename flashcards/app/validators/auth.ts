fd928aed8fa90c1691d5e411f10ab593bdfe4df2
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-03-05 16:22:33 +0100

    Feat(Check if user exists)
    [40mins][DONE]
    - Now checks that user doesn't exist before registering

--- flashcards/app/validators/auth.ts
+++ flashcards/app/validators/auth.ts
@@ -7,4 +7,18 @@ const loginUserValidator = vine.compile(
   })
 )
 
-export { loginUserValidator }
+const registerUserValidator = vine.compile(
+  vine.object({
+    username: vine
+      .string()
+      .trim()
+      .minLength(3)
+      .maxLength(30)
+      .unique({ table: 't_user', column: 'username' }),
+
+    password: vine.string().minLength(8),
+    confirm_password: vine.string().minLength(8).sameAs('password'),
+  })
+)
+
+export { loginUserValidator, registerUserValidator }
